e473d86300930bc2a613e2f8109d75313407fc81
Austin Lam <austinlamcfc@hotmail.com>
2022-07-26 17:58:47 +1000

    new cucumber

--- cypress-cucumber/cypress/e2e/step_definitions/midas-payment.js
+++ cypress-cucumber/cypress/e2e/step_definitions/midas-payment.js
@@ -22,9 +22,6 @@ Given("the user visits billing", (dataTable) => {
   const accountNumber = rows[0][0]
   const invoiceNumber = rows[0][1]
 
-  cy.wrap(accountNumber).as("accountNumber")
-  cy.wrap(invoiceNumber).as("invoiceNumber")
-
   cy.visit(`/en/payment/${accountNumber}/${invoiceNumber}`);
 
   cy.intercept('POST', '/api/authenticate', (req) => {
@@ -33,11 +30,9 @@ Given("the user visits billing", (dataTable) => {
 
 });
 
-Then("invoice should be shown", () => {
+And("should display invoices {string}", (invoiceNumber) => {
 
-  cy.get("@invoiceNumber").then((invoiceNumber) => {
-    cy.get('.billing-document__number', { timeout: 30000 }).first().contains(invoiceNumber)
-  })
+  cy.get('.billing-document__number', { timeout: 30000 }).first().contains(invoiceNumber)
 
   cy.wait('@gqlgetAccountQuery', { timeout: 30000 }).then((interception) => {
     assert.isNotNull(interception.response.body, 'getAccount response is not null')
@@ -46,10 +41,32 @@ Then("invoice should be shown", () => {
 
 })
 
-Then("account should be retrieved", () => {
+When("the user selects the invoice {string}", (invoiceNumber) => {
 
-  cy.intercept('POST', '/api/graphql', (req) => {
-    cy.log('req: ', req)
-  })
+  expect(1).equals(1)
+
+})
+
+And("selects update auto payment and credit card", () => {
+
+  expect(1).equals(1)
+
+})
+
+Then("the hosted payment page is loaded", () => {
+
+  expect(1).equals(1)
+
+})
+
+When("user enters card details and clicks submit", () => {
+
+  expect(1).equals(1)
+
+})
+
+Then("the success page is displayed", () => {
+
+  expect(1).equals(1)
 
 })
